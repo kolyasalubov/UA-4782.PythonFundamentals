07b4e0f7411398bf4342ba05ae01c07d876e6dbe
deep-m00n <277306975+deep-m00n@users.noreply.github.com>
2026-06-06 10:20:01 +0300

    Add solution for 07.1 practical tasks

---
+++ hw/hw07/deep-m00n/task01.py
@@ -0,0 +1,12 @@
+def larger(a, b):
+    """
+    Return the larger of two values.
+
+    Args:
+        a: The first value to compare.
+        b: The second value to compare.
+
+    Returns:
+        The greater of `a` and `b`.
+    """
+    return a if a > b else b
